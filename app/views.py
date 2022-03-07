56e0ead24d8d449b86e9336e393cf6e692555608
Raffique <raffiquemuir@yahoo.com>
2022-03-06 22:49:17 -0500

    100%

--- app/views.py
+++ app/views.py
@@ -10,6 +10,7 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
+from werkzeug.security import check_password_hash
 
 
 ###
@@ -36,24 +37,42 @@ def login():
 
     form = LoginForm()
 
-    if request.method == "POST" and form.validate_on_submit():
+    if request.method == "POST":
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        if form.validate_on_submit():
             # Get the username and password values from the form.
+            username = form.username.data
+            password = form.password.data
 
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
             # You will need to import the appropriate function to do so.
             # Then store the result of that query to a `user` variable so it can be
             # passed to the login_user() method below.
+            user = UserProfile.query.filter_by(username=username).first()
 
-            # get user id, load into session
-            login_user(user)
-
-            # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            if user != None and check_password_hash(user.password, password):
+                # get user id, load into session
+                login_user(user)
+                flash('Login Successful', 'success')
+                # remember to flash a message to the user
+                return redirect(url_for("secure_page"))  # they should be redirected to a secure-page route instead
+            else:
+                flash('Invalid login credentials', 'danger')
     return render_template("login.html", form=form)
+
+@app.route('/secure-page')
+@login_required
+def secure_page():
+    return render_template('secure_page.html')
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash('Logout Successful', 'success')
+    return redirect(url_for('home'))
 
 
 # user_loader callback. This callback is used to reload the user object from
